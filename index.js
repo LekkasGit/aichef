37fc6913be9dca51df6e5d25fa4408e3b4124b43
Lekkas Laptop <alexandros.f.lekkas@gmail.com>
2023-01-10 16:16:44 +0200

    Changed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const time = require("./source/modules/time");
+const time = require("./source/modules/time.js");
+const test = require("./source/modules/test.js")
 
 var express = require("Express");
 var app = express();
 var routes = require("./source/website/routes.js")
 
 console.log(time.prefix() + "Program run");
+console.log(time.prefix() + test.OpenAI());
 
 app.use("/", routes);
 
--- source/discord/testOpenAI.js
+++
@@ -1,6 +0,0 @@
-import { Configuration, Configuration, OpenAIApi } from "openai";
-
-const Configuration = new Configuration ({
-    organization: "org-0YhKwJ8aDxaHW8afdkDp2kyB",
-    apiKey: process.
-})
