8e55a6a5d1cec4f2a7ee92ec452bfc63dfb9217a
Lekkas Laptop <alexandros.f.lekkas@gmail.com>
2023-01-10 11:47:05 +0200

    Updated index file to console log when the program is ran

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 
 const time = require("./source/modules/time");
 
+console.log(time.prefix() + "Program run");
+
 app.get("/", function (req, res) {
     res.send(time.prefix() + "Hello world!")
 })
