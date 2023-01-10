4e2f29990018fbfbe323529e73856bac9988f361
Lekkas Laptop <alexandros.f.lekkas@gmail.com>
2023-01-10 14:09:42 +0200

    Routes js file added to website folder (and fixed)

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var express = require("express");
-var app = express();
-
 const time = require("./source/modules/time");
+
+var express = require("Express");
+var app = express();
+var routes = require("./source/website/routes.js")
 
 console.log(time.prefix() + "Program run");
 
+app.use("/", routes);
+
 app.get("/", function (req, res) {
-    res.send(time.prefix() + "Hello world!")
-})
+    res.send(time.prefix() + "Hello world!");
+});
 
 app.listen(3000);
--- source/website/routes.js
+++ source/website/routes.js
@@ -1,11 +1,14 @@
-var express = require('express');
+const time = require("../modules/time");
+
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res){
-   res.send('GET route on things.');
+router.get('/', function (req, res) {
+   res.send(time.prefix() + "GET route on things");
 });
-router.post('/', function(req, res){
-   res.send('POST route on things.');
+
+router.post('/', function (req, res) {
+   res.send(time.prefix() + "POST route on things");
 });
 
 module.exports = router;
