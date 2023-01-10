a03414df36a4abcd7dd42d66fd729623cdfd9a29
Lekkas Laptop <alexandros.f.lekkas@gmail.com>
2023-01-10 15:00:25 +0200

    Updated a lot of files

---
+++ source/discord/testOpenAI.js
@@ -0,0 +1,6 @@
+import { Configuration, Configuration, OpenAIApi } from "openai";
+
+const Configuration = new Configuration ({
+    organization: "org-0YhKwJ8aDxaHW8afdkDp2kyB",
+    apiKey: process.
+})
