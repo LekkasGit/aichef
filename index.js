eeaa920cf21e63ba91a6d0e451f3fe21d5b29ba9
Lekkas Laptop <alexandros.f.lekkas@gmail.com>
2023-01-10 08:52:58 +0200

    Index updated

--- index.js
+++ index.js
@@ -6,20 +6,6 @@ app.set('views', "./views")
 
 app.use(express.static('public'));
 
-/*
-app.get('/first_template', function(req, res){
-    res.render('first_view');
-});
-*/
-/*
-app.get('/dynamic_view', function(req, res){
-    res.render('dynamic', {
-        name: "TutorialPoint",
-        lol: "lol?"
-    });
-});
-*/
-
 app.get('/components', function(req,res) {
     res.render('home');
 });
